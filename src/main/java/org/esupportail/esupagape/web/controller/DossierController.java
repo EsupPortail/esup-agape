0338576c061bdc141587d0036e09356e686b59ab
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-09-20 14:23:12 +0200

    ajout show dossier

--- src/main/java/org/esupportail/esupagape/web/controller/DossierController.java
+++ src/main/java/org/esupportail/esupagape/web/controller/DossierController.java
@@ -1,9 +1,25 @@
 package org.esupportail.esupagape.web.controller;
 
+import org.esupportail.esupagape.repository.DossierRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/dossiers")
 public class DossierController {
+
+    @Resource
+    private DossierRepository dossierRepository;
+
+    @GetMapping("{id}")
+    public String show(@RequestParam Long id, Model model) {
+        model.addAttribute("dossier", dossierRepository.findById(id).get());
+        return "dossier/show";
+    }
+
 }
