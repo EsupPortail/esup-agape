72d48f0f0140159af549d120bc2987ce5023f268
Fabienne Bergès <bergefab@dgs-22-27b6-1752.univ-rouen.fr>
2022-09-20 16:53:16 +0200

    Add getAllIndividus

--- src/main/java/org/esupportail/esupagape/service/IndividuService.java
+++ src/main/java/org/esupportail/esupagape/service/IndividuService.java
@@ -1,17 +1,23 @@
 package org.esupportail.esupagape.service;
 
 import org.esupportail.esupagape.entity.Individu;
+import org.esupportail.esupagape.exception.AgapeException;
 import org.esupportail.esupagape.repository.IndividuRepository;
 import org.esupportail.esupagape.service.interfaces.importIndividu.IndividuSourceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IndividuService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final List<IndividuSourceService> individuSourceServices;
 
     @Resource
@@ -21,17 +27,35 @@ public class IndividuService {
         this.individuSourceServices = individuSourceServices;
     }
 
+    public List<Individu> getAllIndividus(){
+        List<Individu> individus = new ArrayList<>();
+        individuRepository.findAll().forEach(individus::add);
+        return individus;
+    }
     @Transactional
-    public void importIndividus() {
+    public Object importIndividus() {
         for (IndividuSourceService individuSourceService : individuSourceServices) {
             List<Individu> individus = individuSourceService.getAllIndividuNums();
             for (Individu individu : individus) {
                 Individu individu1 = individuRepository.findByNumEtu(individu.getNumEtu());
-                if(individu1 == null) {
+                if (individu1 == null) {
                     individuRepository.save(individu);
                 }
             }
         }
+        return null;
     }
 
+    public Individu findById(Long id) throws AgapeException {
+        Optional<Individu> optionalIndividu = individuRepository.findById(id);
+        if(optionalIndividu.isPresent()) {
+            return optionalIndividu.get();
+        } else {
+            throw new AgapeException("Je n'ai pas trouvé cet individu");
+        }
+    }
+
+    public void save(Individu individu) {
+        individuRepository.save(individu);
+    }
 }
