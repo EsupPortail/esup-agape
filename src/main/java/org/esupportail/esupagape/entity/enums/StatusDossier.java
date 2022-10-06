035166dd5e5ef18d57b01b04a0b22020131ce2ea
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2022-10-06 14:46:53 +0200

    Changement valeurs enums filtres, vues list et show

--- src/main/java/org/esupportail/esupagape/entity/enums/StatusDossier.java
+++ src/main/java/org/esupportail/esupagape/entity/enums/StatusDossier.java
@@ -1,10 +1,20 @@
 package org.esupportail.esupagape.entity.enums;
 
 public enum StatusDossier {
-    IMPORTE,
-    ACCUEILLI_ENTRETIEN_PHYSIQUE,
-    ACCUEILLI_ENTRETIEN_TELEPHONIQUE_MAIL,
-    SUIVI,
-    IMPOSSIBLE_A_CONTACTER,
-    RECU_PAR_LA_MEDECINE_PREVENTIVE
+    IMPORTE("Importé"),
+    ACCUEILLI_ENTRETIEN_PHYSIQUE("Accueilli en entretien physique"),
+    ACCUEILLI_ENTRETIEN_TELEPHONIQUE_MAIL("Accueilli en entretien à distance"),
+    SUIVI("Suivi"),
+    IMPOSSIBLE_A_CONTACTER("Impossible à contacter"),
+    RECU_PAR_LA_MEDECINE_PREVENTIVE("Reçu par la médecine préventive");
+
+    private final String displayValue;
+
+    private StatusDossier(String displayValue){
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
--- src/main/java/org/esupportail/esupagape/entity/enums/TypeIndividu.java
+++ src/main/java/org/esupportail/esupagape/entity/enums/TypeIndividu.java
@@ -2,6 +2,15 @@ package org.esupportail.esupagape.entity.enums;
 
 public enum TypeIndividu {
 
-    LYCEEN, ETUDIANT, HORS_UNIV
+    LYCEEN("Lycéen"), ETUDIANT("Etudiant"), HORS_UNIV("Hors université");
 
+    private final String displayValue;
+
+    private TypeIndividu(String displayValue){
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
