7c9ecef454b985a5a6f944db9ff76aa047407587
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2022-10-19 15:51:12 +0200

    Ajouts boutons actions voir update delete contact; todo fonctionnement

--- src/main/java/org/esupportail/esupagape/service/ContactService.java
+++ src/main/java/org/esupportail/esupagape/service/ContactService.java
@@ -1,12 +1,14 @@
 package org.esupportail.esupagape.service;
 
 import org.esupportail.esupagape.entity.Contact;
+import org.esupportail.esupagape.exception.AgapeException;
 import org.esupportail.esupagape.repository.ContactRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
@@ -28,8 +30,17 @@ public class ContactService {
         contactRepository.save(contact);
     }
 
+    public Contact findById(Long id) throws AgapeException {
+        Optional<Contact> optionalContact = contactRepository.findById(id);
+        if (optionalContact.isPresent()) {
+            return optionalContact.get();
+        } else {
+            throw  new AgapeException("Je n'ai pas trouvé ce contact");
+        }
+    }
+
     @Transactional
-    public void deleteContact (long id) {
+    public void deleteContact(long id) {
         this.contactRepository.deleteById(id);
     }
 
--- src/main/java/org/esupportail/esupagape/web/controller/ContactController.java
+++ src/main/java/org/esupportail/esupagape/web/controller/ContactController.java
@@ -1,17 +1,18 @@
 package org.esupportail.esupagape.web.controller;
 
 import org.esupportail.esupagape.entity.Contact;
-import org.esupportail.esupagape.entity.Dossier;
-import org.esupportail.esupagape.entity.Individu;
+import org.esupportail.esupagape.exception.AgapeException;
 import org.esupportail.esupagape.service.ContactService;
-import org.esupportail.esupagape.service.DossierService;
-import org.esupportail.esupagape.service.IndividuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -31,4 +32,32 @@ public class ContactController {
         return "contacts/list";
     }
 
+    @GetMapping("/show")
+    public String showContact(@PathVariable Long id, Model model) {
+        List<Contact> contacts = contactService.getContactsByDossier(id);
+        model.addAttribute("contacts", contacts);
+        return "contacts/show";
+    }
+
+    @GetMapping("/update")
+
+    public String updateContact(@PathVariable(value = "id") long id, Model model) throws AgapeException {
+        Contact contact = contactService.findById(id);
+        model.addAttribute("contact", contact);
+        return "contacts/update";
+    }
+ @PutMapping("/contacts")
+    public String update(@Valid Contact contact, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "contacts/update";
+        }
+        contactService.save(contact);
+        return "redirect:/contacts";
+ }
+
+    @DeleteMapping(value = "/delete")
+    public String deleteDossier(@PathVariable("id") long id) {
+        contactService.deleteContact(id);
+        return "redirect:/contacts";
+    }
 }
