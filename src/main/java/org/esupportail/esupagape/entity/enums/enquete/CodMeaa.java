3a94c1709dc67dc08798cc9d84708a1629b73dd5
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2022-11-12 22:54:14 +0100

    fix enum codMeaa

--- src/main/java/org/esupportail/esupagape/entity/enums/enquete/CodMeaa.java
+++ src/main/java/org/esupportail/esupagape/entity/enums/enquete/CodMeaa.java
@@ -2,18 +2,8 @@ package org.esupportail.esupagape.entity.enums.enquete;
 
 public enum CodMeaa {
 
-    AA1("l'étudiant est reçu ponctuellement par la structure ou le responsable handicap de l’établissement"),
-    AA2("l'étudiant est suivi par la structure ou le responsable handicap de l’établissement"),
-    AA3("recours à un matériel ou support spécialement adapté au besoin de l'étudiant handicapé"),
-    AAo("autre (à préciser)");
-
-    private final String libelle;
-
-    private CodMeaa(String value) {
-        libelle = value;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
+    AA1,
+    AA2,
+    AA3,
+    AAo
 }
