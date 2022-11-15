b84694ec75d8efff82776c20fc728e96ff4d9a6d
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2022-11-15 14:36:29 +0100

    Début initialisation enquete (create)

--- src/main/java/org/esupportail/esupagape/service/EnqueteService.java
+++ src/main/java/org/esupportail/esupagape/service/EnqueteService.java
@@ -6,7 +6,6 @@ import org.esupportail.esupagape.repository.EnqueteRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,11 +21,11 @@ public class EnqueteService {
     }
 
 
-    @Transactional
+   /* @Transactional
     public List<Enquete> findEntretiensByDossierId(Long dossierId) {
         List<Enquete> enquetes = enqueteRepository.findEnquetesByDossierId(dossierId);
         return enquetes;
-    }
+    } */
 
     public Enquete getById(Long id) throws AgapeJpaException {
         Optional<Enquete> optionalEnquete = enqueteRepository.findById(id);
@@ -38,13 +37,14 @@ public class EnqueteService {
     }
 
     @Transactional
-    public void save(Enquete enquete) {
+    public void create(Enquete enquete) {
         enqueteRepository.save(enquete);
     }
 
     @Transactional
-    public void updateEnquete(Long id, Enquete enquete) throws AgapeJpaException {
+    public void save(Long id, Enquete enquete) throws AgapeJpaException {
         Enquete enqueteToUpdate = getById(id);
+        enqueteRepository.save(enquete);
         enqueteToUpdate.setId(enqueteToUpdate.getId());
         enqueteToUpdate.setNfic(enquete.getNfic());
         enqueteToUpdate.setNumetu(enquete.getNumetu());
@@ -74,5 +74,41 @@ public class EnqueteService {
         enqueteToUpdate.setDjaCop(enquete.getDjaCop());
         enqueteToUpdate.setNewNum(enquete.getNewNum());
         enqueteToUpdate.setNewId(enquete.getNewId());
+        enqueteRepository.save(enquete);
     }
+
+   /* @Transactional
+    public void updateEnquete(Long id, Enquete enquete) throws AgapeJpaException {
+        Enquete enqueteToUpdate = getById();
+        enqueteToUpdate.setId(enqueteToUpdate.getId());
+        enqueteToUpdate.setNfic(enquete.getNfic());
+        enqueteToUpdate.setNumetu(enquete.getNumetu());
+        enqueteToUpdate.setAn(enqueteToUpdate.getAn());
+        enqueteToUpdate.setCivilite(enquete.getCivilite());
+        enqueteToUpdate.setTypeFrmn(enquete.getTypeFrmn());
+        enqueteToUpdate.setModFrmn(enquete.getModFrmn());
+        enqueteToUpdate.setCodSco(enquete.getCodSco());
+        enqueteToUpdate.setCodFmt(enquete.getCodFmt());
+        enqueteToUpdate.setCodFil(enquete.getCodFil());
+        enqueteToUpdate.setCodHd(enquete.getCodHd());
+        enqueteToUpdate.setHdTmp(enquete.getHdTmp());
+        enqueteToUpdate.setCom(enquete.getCom());
+        enqueteToUpdate.setCodPfpp(enquete.getCodPfpp());
+        enqueteToUpdate.setCodPfas(enquete.getCodPfas());
+        enqueteToUpdate.setCodMeahF(enquete.getCodMeahF());
+        enqueteToUpdate.setInterpH(enquete.getInterpH());
+        enqueteToUpdate.setCodAmL(enquete.getCodAmL());
+        enqueteToUpdate.setCodMeahF(enquete.getCodMeahF());
+        enqueteToUpdate.setCodeurH(enquete.getCodeurH());
+        enqueteToUpdate.setAidHNat(enquete.getAidHNat());
+        enqueteToUpdate.setCodMeae(enquete.getCodMeae());
+        enqueteToUpdate.setAutAE(enquete.getAutAE());
+        enqueteToUpdate.setCodMeaa(enquete.getCodMeaa());
+        enqueteToUpdate.setAutAA(enquete.getAutAA());
+        enqueteToUpdate.setCodAmL(enquete.getCodAmL());
+        enqueteToUpdate.setDjaCop(enquete.getDjaCop());
+        enqueteToUpdate.setNewNum(enquete.getNewNum());
+        enqueteToUpdate.setNewId(enquete.getNewId());
+        enqueteRepository.save(enquete);
+    }*/
 }
--- src/main/java/org/esupportail/esupagape/web/controller/EnqueteController.java
+++ src/main/java/org/esupportail/esupagape/web/controller/EnqueteController.java
@@ -1,5 +1,6 @@
 package org.esupportail.esupagape.web.controller;
 
+import org.esupportail.esupagape.entity.Dossier;
 import org.esupportail.esupagape.entity.Enquete;
 import org.esupportail.esupagape.entity.enums.Civilite;
 import org.esupportail.esupagape.entity.enums.enquete.CodAmL;
@@ -14,13 +15,20 @@ import org.esupportail.esupagape.entity.enums.enquete.CodPfpp;
 import org.esupportail.esupagape.entity.enums.enquete.CodSco;
 import org.esupportail.esupagape.entity.enums.enquete.ModFrmn;
 import org.esupportail.esupagape.entity.enums.enquete.TypeFrmn;
+import org.esupportail.esupagape.exception.AgapeJpaException;
+import org.esupportail.esupagape.service.EnqueteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.lang.invoke.MethodHandles;
 
 @Controller
@@ -30,6 +38,11 @@ public class EnqueteController {
 
     public static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private final EnqueteService enqueteService;
+
+    public EnqueteController(EnqueteService enqueteService) {
+        this.enqueteService = enqueteService;
+    }
     /*@GetMapping
     public String list(@PathVariable Long id,Dossier dossier, Model model) throws AgapeJpaException {
         List<Enquete> enquetes = enqueteService.findEntretiensByDossierId(id);
@@ -39,8 +52,8 @@ public class EnqueteController {
     }*/
 
     @GetMapping
-    public String create(Model model) {
-        Enquete enquete = new Enquete();
+    public String create(Dossier dossier, Model model) {
+        Enquete enquete = dossier.getEnquete();
         model.addAttribute("enquete", enquete);
         model.addAttribute("typeFrmns", TypeFrmn.values());
         model.addAttribute("modFrmns", ModFrmn.values());
@@ -58,4 +71,33 @@ public class EnqueteController {
         return "enquetes/create";
     }
 
+    @PostMapping("/create")
+    public String create(@Valid Enquete enquete, BindingResult bindingResult, Dossier dossier, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("enquete", enquete);
+            model.addAttribute("typeFrmns", TypeFrmn.values());
+            model.addAttribute("modFrmns", ModFrmn.values());
+            model.addAttribute("codFils", CodFil.values());
+            model.addAttribute("codFmts", CodFmt.values());
+            model.addAttribute("codScos", CodSco.values());
+            model.addAttribute("codHds", CodHd.values());
+            model.addAttribute("codPfpps", CodPfpp.values());
+            model.addAttribute("codPfass", CodPfas.values());
+            model.addAttribute("codMeahFs", CodMeahF.values());
+            model.addAttribute("codMeaes", CodMeae.values());
+            model.addAttribute("codMeaas", CodMeaa.values());
+            model.addAttribute("codamls", CodAmL.values());
+            model.addAttribute("civilites", Civilite.values());
+            return "enquetes/create";
+        }
+        enquete.setDossier(dossier);
+        enqueteService.create(enquete);
+        return "redirect:/dossiers/" + dossier.getId() + "/enquetes";
+    }
+
+    @PutMapping("/{enqueteId}/update")
+    public String updateEnquete(@PathVariable Long enqueteId, @Valid Enquete enquete, Dossier dossier) throws AgapeJpaException {
+        enqueteService.save(enqueteId, enquete);
+        return "redirect:/dossiers/" + dossier.getId() + "/enquetes";
+    }
 }
