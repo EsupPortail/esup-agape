c3670428dcfabfb770a92e241f072c83df4cb787
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2023-01-04 09:45:55 +0100

    Check at least one checkbox is selected

--- src/main/resources/static/js/agape.js
+++ src/main/resources/static/js/agape.js
@@ -193,3 +193,35 @@ function textAreaAdjust(element, lineHeight) {
     let count = lines.length;
     element.style.height = (lineHeight * count) + "px";
 }
+
+// Check at least one checkbox is selected
+
+(function () {
+    const form = document.querySelector('#amenagementForm');
+const checkboxes = form.querySelectorAll('input[type=checkbox]');
+const checkboxLength = checkboxes.length;
+const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+
+function init() {
+    if (firstCheckbox) {
+        for (let i = 0; i < checkboxLength; i++) {
+            checkboxes[i].addEventListener('change', checkValidity);
+        }
+
+        checkValidity();
+    }
+}
+
+function isChecked() {
+    for (let i = 0; i < checkboxLength; i++) {
+        if(checkboxes[i].checked) return true;
+    }
+    return false;
+}
+
+function checkValidity() {
+    const errorMessage = !isChecked() ? 'Au moins une épreuve doit être sélectionnée pour poursuivre.' : '';
+    firstCheckbox.setCustomValidity(errorMessage)
+}
+init();
+})();
