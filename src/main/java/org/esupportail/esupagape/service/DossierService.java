d835c8c9eb51f0e0ef99f75e42dfb2819a137976
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-01-25 14:47:19 +0100

    revert 2021

--- src/main/java/org/esupportail/esupagape/service/DossierService.java
+++ src/main/java/org/esupportail/esupagape/service/DossierService.java
@@ -146,7 +146,7 @@ public class DossierService {
     @Transactional
     public void syncAllDossiers() {
         logger.info("Sync dossiers started");
-        List<Dossier> dossiers = dossierRepository.findAllByYear(2021, Pageable.unpaged()).getContent();
+        List<Dossier> dossiers = dossierRepository.findAllByYear(utilsService.getCurrentYear(), Pageable.unpaged()).getContent();
         for (Dossier dossier : dossiers) {
             syncDossier(dossier.getId());
         }
@@ -160,7 +160,7 @@ public class DossierService {
             dossier.setStatusDossierAmenagement(StatusDossierAmenagement.NON);
         }
         for (DossierInfosService dossierInfosService : dossierInfosServices) {
-            DossierInfos dossierInfos = dossierInfosService.getDossierProperties(dossier.getIndividu(), 2021, false, false, new DossierInfos());
+            DossierInfos dossierInfos = dossierInfosService.getDossierProperties(dossier.getIndividu(), dossier.getYear(), false, false, new DossierInfos());
             if (dossierInfos != null) {
                 if (StringUtils.hasText(dossierInfos.getEtablissement())) {
                     dossier.setSite(dossierInfos.getEtablissement());
