23456e9e757831194fa0461785d684301207e32e
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2023-03-21 15:49:19 +0100

    Suppression PJ entretien

--- src/main/java/org/esupportail/esupagape/repository/EntretienRepository.java
+++ src/main/java/org/esupportail/esupagape/repository/EntretienRepository.java
@@ -1,26 +1,12 @@
 package org.esupportail.esupagape.repository;
 
-import org.esupportail.esupagape.dtos.EntretienAttachementDto;
 import org.esupportail.esupagape.entity.Entretien;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EntretienRepository extends JpaRepository <Entretien, Long> {
 
     Page<Entretien> findEntretiensByDossierId(Long dossierId, Pageable pageable);
-
-    @Query("select e.id as id, " +
-            "e.date as date, " +
-            "e.typeContact as typeContact, " +
-            "e.interlocuteur as interlocuteur, " +
-            "e.compteRendu as compteRendu, " +
-            "coalesce(count(d.id), 0) as numberOfAttachements " +
-            "from Entretien e " +
-            "left join Document d on e.id = d.parentId " +
-            "where e.dossier.id = :dossierId " +
-            "group by e.id")
-    Page<EntretienAttachementDto> findEntretiensWithAttachementsByDossierId(Long dossierId, Pageable pageable);
 
 }
--- src/main/java/org/esupportail/esupagape/web/controller/EntretienController.java
+++ src/main/java/org/esupportail/esupagape/web/controller/EntretienController.java
@@ -1,11 +1,8 @@
 package org.esupportail.esupagape.web.controller;
 
-import org.esupportail.esupagape.dtos.EntretienAttachementDto;
-import org.esupportail.esupagape.entity.Dossier;
 import org.esupportail.esupagape.entity.Entretien;
 import org.esupportail.esupagape.entity.enums.TypeContact;
 import org.esupportail.esupagape.exception.AgapeException;
-import org.esupportail.esupagape.exception.AgapeIOException;
 import org.esupportail.esupagape.exception.AgapeJpaException;
 import org.esupportail.esupagape.service.DocumentService;
 import org.esupportail.esupagape.service.EntretienService;
@@ -14,15 +11,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
@@ -44,7 +42,7 @@ public class EntretienController {
     public String list(@PathVariable Long dossierId, @PageableDefault(
             sort = "date",
             direction = Sort.Direction.DESC) Pageable pageable, Model model) {
-        Page<EntretienAttachementDto> entretiens = entretienService.findEntretiensWithAttachementsByDossierId(dossierId,pageable);
+        Page<Entretien> entretiens = entretienService.findByDossier(dossierId, pageable);
         model.addAttribute("entretiens", entretiens);
         model.addAttribute("entretien", new Entretien());
         model.addAttribute("typeContacts", Arrays.asList(TypeContact.values()));
@@ -78,7 +76,6 @@ public class EntretienController {
     public String updateEntretien(@PathVariable Long entretienId, Model model) throws AgapeException {
         Entretien entretien = entretienService.getById(entretienId);
         model.addAttribute("entretien", entretien);
-        model.addAttribute("attachments", entretienService.getAttachements(entretienId));
         model.addAttribute("typeContacts", TypeContact.values());
         return "entretiens/update";
     }
@@ -94,24 +91,4 @@ public class EntretienController {
         entretienService.deleteEntretien(entretienId);
         return "redirect:/dossiers/" + dossierId + "/entretiens";
     }
-
-    @PostMapping("/{entretienId}/add-attachments")
-    public String addAttachments(@PathVariable Long dossierId, @PathVariable Long entretienId, @RequestParam("multipartFiles") MultipartFile[] multipartFiles) throws AgapeException {
-        entretienService.addAttachment(entretienId, multipartFiles);
-        return "redirect:/dossiers/" + dossierId + "/entretiens/" + entretienId + "/update";
-    }
-
-    @GetMapping(value = "/{entretienId}/get-attachment/{attachmentId}")
-    @ResponseBody
-    public ResponseEntity<Void> getLastFileFromSignRequest(@PathVariable("attachmentId") Long attachmentId, HttpServletResponse httpServletResponse) throws AgapeIOException {
-        documentService.getDocumentHttpResponse(attachmentId, httpServletResponse);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping(value = "/{entretienId}/delete-attachment/{attachmentId}")
-    public String getLastFileFromSignRequest(@PathVariable("entretienId") Long entretienId, @PathVariable("attachmentId") Long attachmentId, Dossier dossier) throws AgapeException {
-        entretienService.deleteAttachment(entretienId, attachmentId);
-        return "redirect:/dossiers/" + dossier.getId() + "/entretiens/" + entretienId + "/update";
-    }
-
 }
