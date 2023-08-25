de4deacf63b1e4415cd24056a6f4a6ee277da562
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-08-25 11:11:47 +0200

    enable scheduler

--- src/main/java/org/esupportail/esupagape/service/scheduler/SchedulerService.java
+++ src/main/java/org/esupportail/esupagape/service/scheduler/SchedulerService.java
@@ -7,10 +7,11 @@ import org.esupportail.esupagape.service.DossierService;
 import org.esupportail.esupagape.service.IndividuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-//@EnableScheduling
+@EnableScheduling
 @Service
 public class SchedulerService {
 
