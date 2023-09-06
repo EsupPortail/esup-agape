9ae6e4d2e18bcc566985e56a4d4958ca26bcf280
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-09-06 12:33:35 +0200

    fix display avis medecin

--- src/main/java/org/esupportail/esupagape/web/controller/AmenagementController.java
+++ src/main/java/org/esupportail/esupagape/web/controller/AmenagementController.java
@@ -138,10 +138,10 @@ public class AmenagementController {
 
     @GetMapping(value = "/{amenagementId}/get-avis", produces = "application/zip")
     @ResponseBody
-    public ResponseEntity<Void> getAvis(@PathVariable("amenagementId") Long amenagementId, HttpServletResponse httpServletResponse) throws IOException, AgapeException {
+    public ResponseEntity<Void> getAvis(@PathVariable("amenagementId") Long amenagementId, @RequestParam String disposition, HttpServletResponse httpServletResponse) throws IOException, AgapeException {
         httpServletResponse.setContentType("application/pdf");
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"certificat_" + amenagementId + ".pdf\"");
+        httpServletResponse.setHeader("Content-Disposition", disposition + "; filename=\"certificat_" + amenagementId + ".pdf\"");
         amenagementService.getAvis(amenagementId, httpServletResponse);
         httpServletResponse.flushBuffer();
         return new ResponseEntity<>(HttpStatus.OK);
