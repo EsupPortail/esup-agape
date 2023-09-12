e3c774dd84a4871fddf529a5904c49015da4d60e
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2023-09-12 16:39:14 +0200

    Couleurs statuts dossiers sur toute la ligne close#117

--- src/main/java/org/esupportail/esupagape/entity/enums/StatusDossier.java
+++ src/main/java/org/esupportail/esupagape/entity/enums/StatusDossier.java
@@ -1,24 +1,27 @@
 package org.esupportail.esupagape.entity.enums;
 
 public enum StatusDossier {
-    IMPORTE("rgba(125, 125, 218, 1)", "rgba(125, 125, 218, 0.3)"),
-    AJOUT_MANUEL("", ""),
-    RECU_PAR_LA_MEDECINE_PREVENTIVE("", ""),
-    RECONDUIT("", ""),
-    NON_RECONDUIT("", ""),
-    ACCUEILLI("", ""),
-    SUIVI("", ""),
-    IMPOSSIBLE_A_CONTACTER("", ""),
-    ANONYMOUS("", "");
+    IMPORTE("rgb(208,201,226)", "rgba(208,201,226, 0.3)", ""),
+    AJOUT_MANUEL("rgb(61,133,198)", "rgba(61,133,198, 0.2)", ""),
+    RECU_PAR_LA_MEDECINE_PREVENTIVE("rgb(205,247,249) ", "rgba(205,247,249, 0.3)", "#777777"),
+    RECONDUIT("rgb(255,191,151)", "rgba(255,191,151, 0.3)", ""),
+    NON_RECONDUIT("rgb(248,215,253)", "rgba(248,215,253, 0.3)", ""),
+    ACCUEILLI("rgb(255,253,204)", "rgba(255,253,204, 0.3)", "#777777"), 
+    SUIVI("rgb(182,215,168)", "rgba(182,215,168, 0.3)", ""),
+    IMPOSSIBLE_A_CONTACTER("rgb(234,153,153)", "rgba(234,153,153, 0.3)", ""),
+    ANONYMOUS("rgb(225,221,210)", "rgba(225,221,210, 0.3)", "");
     //DESINSCRIT
 
     final String badgeColor;
 
     final String backgroundColor;
 
-    StatusDossier(String badgeColor, String backgroundColor) {
+    final String textColor;
+
+    StatusDossier(String badgeColor, String backgroundColor, String textColor) {
         this.badgeColor = badgeColor;
         this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
     }
 
     public String getBadgeColor() {
@@ -27,5 +30,9 @@ public enum StatusDossier {
 
     public String getBackgroundColor() {
         return backgroundColor;
+    }
+
+    public String getTextColor() {
+        return textColor;
     }
 }
