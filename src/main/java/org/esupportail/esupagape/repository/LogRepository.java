9129b0a089a33a6fe5060f70c2b2ad38efaa314c
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-11-14 16:29:57 +0100

    add logs in view

--- src/main/java/org/esupportail/esupagape/repository/LogRepository.java
+++ src/main/java/org/esupportail/esupagape/repository/LogRepository.java
@@ -3,5 +3,8 @@ package org.esupportail.esupagape.repository;
 import org.esupportail.esupagape.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LogRepository extends JpaRepository <Log, Long> {
+    List<Log> findByDossierId(Long dossierId);
 }
